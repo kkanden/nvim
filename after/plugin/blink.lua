03ffa2c869a4210ca29101c6900eb1f6185fcdfe
oliwia <24637207+kkanden@users.noreply.github.com>
2025-01-04 18:11:11 +0100

    remove cmp_r to use lsp native completion for R

--- after/plugin/blink.lua
+++ after/plugin/blink.lua
@@ -1,8 +1,8 @@
 require("blink.cmp").setup({
     keymap = {
         ["<C-b>"] = { "select_and_accept" },
-        ['<C-s>'] = { 'scroll_documentation_up', 'fallback' },
-        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+        ["<C-s>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
     },
     completion = {
         menu = {
@@ -10,27 +10,31 @@ require("blink.cmp").setup({
                 columns = {
                     { "kind_icon",   "label", "label_description", gap = 1 },
                     { "kind",        gap = 6 },
-                    { "source_name", gap = 1 }
-                }
-            }
+                    { "source_name", gap = 1 },
+                },
+            },
         },
         documentation = {
             auto_show = true,
-        }
+        },
     },
     signature = {
         enabled = true,
     },
     sources = {
         default = {
-            'lsp', 'path', 'snippets', 'buffer', 'cmp_r'
+            "lsp",
+            "path",
+            "snippets",
+            "buffer",
+            --     'cmp_r'
         },
-        providers = {
-            cmp_r = {
-                name = "cmp_r",
-                module = 'blink.compat.source',
-            }
-        }
+        -- providers = {
+        --     cmp_r = {
+        --         name = "cmp_r",
+        --         module = 'blink.compat.source',
+        --     }
+        -- }
         -- cmdline = {},
     },
 })
