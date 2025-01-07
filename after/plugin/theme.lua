f1bc4d96797d3c6c6f8c6d7ed1a92eebb481b77c
oliwia <24637207+kkanden@users.noreply.github.com>
2025-01-07 22:13:14 +0100

    change WinSeparator color, clear up theme

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -84,27 +84,14 @@ require("kanagawa").setup({
         local theme = colors.theme
         local palette = colors.palette
         return {
-            -- FloatBorder = { bg = "#211924" },
-            -- FloatTitle = { bg = "#211924" },
+            WinSeparator = { fg = palette.fujiGray },
 
-            -- Save an hlgroup with dark background and dimmed foreground
-            -- so that you can use it where your still want darker windows.
-            -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
             NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
 
             -- Popular plugins that open floats will link to NormalFloat by default;
             -- set their background accordingly if you wish to keep them dark and borderless
             LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
             MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-
-            -- borderless Telescope
-            -- TelescopeTitle = { fg = theme.ui.special, bold = true },
-            -- TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-            -- TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-            -- TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-            -- TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-            -- TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-            -- TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
 
             TelescopeBorder = { bg = "#AE7FA7", fg = "#000000" },
             TelescopeNormal = { bg = "#AE7FA7", fg = "#000000" },
@@ -126,26 +113,11 @@ require("kanagawa").setup({
             AlphaShortcut = { fg = palette.oniViolet },
             AlphaFooter = { fg = palette.fujiGray },
 
-            -- cmp kinds
-            -- Cmp
-            -- CmpDocumentation = {},
-            -- CmpDocumentationBorder = {},
-
-            -- CmpItemAbbr = {},
-            -- CmpItemAbbrDeprecated = {},
-            -- CmpItemAbbrMatch = {},
-            -- CmpItemAbbrMatchFuzzy = {},
-
-            -- CmpItemMenu = {},
-            --
-            -- CmpItemKindDefault = {},
-
             CmpItemKindKeyword = { fg = palette.lightBlue },
 
             CmpItemKindVariable = { fg = palette.oniViolet },
             CmpItemKindConstant = { fg = palette.lotusViolet4 },
             CmpItemKindReference = { fg = palette.oniViolet },
-            -- CmpItemKindValue = {},
             CmpItemKindCopilot = { fg = palette.dragonTeal },
 
             CmpItemKindFunction = { fg = palette.springBlue },
@@ -167,15 +139,6 @@ require("kanagawa").setup({
             CmpItemKindEnumMember = { fg = palette.lotusGreen },
             CmpItemKindOperator = { fg = palette.springGreen },
             CmpItemKindSnippet = { fg = palette.fujiGray },
-
-            -- IblRed = { fg = palette.dragonRed },
-            -- IblYellow = { fg = palette.dragonYellow },
-            -- IblBlue = { fg = palette.dragonBlue },
-            -- IblOrange = { fg = palette.dragonOrange },
-            -- IblGreen = { fg = palette.dragonGreen },
-            -- -- IblViolet = { fg = palette.dragonViolet},
-            -- -- IblCyan = { fg = palette.dragonTeal },
-            -- IblAqua = { fg = palette.dragonAqua },
         }
     end,
 })
