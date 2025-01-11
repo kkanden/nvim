f1f07998886d137e2b55e905111583e85cf5864c
oliwia <24637207+kkanden@users.noreply.github.com>
2025-01-11 01:05:37 +0100

    remove redundant extra ruff in LSP

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -107,7 +107,6 @@ local ensure_installed = vim.tbl_keys(servers or {})
 vim.list_extend(ensure_installed, {
     "stylua",
     "mypy",
-    "ruff",
     "black",
 })
 require("mason-tool-installer").setup({
