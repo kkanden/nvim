1e51628b5f20747d15839b0deeae902e38ecde31
oliwia <24637207+kkanden@users.noreply.github.com>
2025-01-11 15:00:05 +0100

    disable ruff becasue error

--- after/plugin/nullls.lua
+++ after/plugin/nullls.lua
@@ -4,6 +4,6 @@ null_ls.setup({
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.black,
         null_ls.builtins.diagnostics.mypy,
-        null_ls.builtins.diagnostics.ruff,
+        -- null_ls.builtins.diagnostics.ruff,
     },
 })
