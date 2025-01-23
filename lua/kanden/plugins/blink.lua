9bb6e2df7126c33ada948bda4b59743261eaa595
oliwia <24637207+kkanden@users.noreply.github.com>
2025-01-23 23:31:59 +0100

    add lazydev to blink.cmp

--- lua/kanden/plugins/blink.lua
+++ lua/kanden/plugins/blink.lua
@@ -27,6 +27,15 @@ require("blink.cmp").setup({
             "path",
             "snippets",
             "buffer",
+            "lazydev",
+        },
+        providers = {
+            lazydev = {
+                name = "LazyDev",
+                module = "lazydev.integrations.blink",
+                -- make lazydev completions top priority (see `:h blink.cmp`)
+                score_offset = 100,
+            },
         },
     },
 })
