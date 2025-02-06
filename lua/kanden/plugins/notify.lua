080f4c1f9d0b42d4b987a9242746b4f0ba919f2d
oliwia <24637207+kkanden@users.noreply.github.com>
2025-02-06 14:57:38 +0100

    nvim-notify: change style to wrapped-compact

--- lua/kanden/plugins/notify.lua
+++ lua/kanden/plugins/notify.lua
@@ -3,7 +3,7 @@ vim.notify = require("notify")
 require("notify").setup({
     timeout = 3000,
     max_width = math.floor(vim.o.columns * 0.3),
-    render = "compact",
+    render = "wrapped-compact",
     fps = 120,
     top_down = false,
     stages = "static",
