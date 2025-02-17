78ecb2e4b04046022a23313b7f8cd08ee9b590f5
oliwia <24637207+kkanden@users.noreply.github.com>
2025-02-17 11:28:41 +0100

    make relative paths in harpoon consistent

--- lua/kanden/plugins/harpoon.lua
+++ lua/kanden/plugins/harpoon.lua
@@ -1,6 +1,10 @@
 local map = require("kanden.lib").map
 local harpoon = require("harpoon")
 local job = require("plenary.job")
+local Path = require("plenary.path")
+local function normalize_path(buf_name, root)
+    return Path:new(buf_name):make_relative(root)
+end
 
 local function get_os_command_output(cmd, cwd)
     if type(cmd) ~= "table" then return {} end
@@ -34,6 +38,35 @@ harpoon:setup({
     settings = {
         save_on_toggle = true,
         key = git_branch_marks,
+    },
+    default = {
+        -- modified original code to make relative paths consistent
+        get_root_dir = function() return vim.fn.getcwd() end,
+
+        ---@param config HarpoonPartialConfigItem
+        ---@param name? any
+        ---@return HarpoonListItem
+        create_list_item = function(config, name)
+            name = normalize_path(
+                vim.api
+                    .nvim_buf_get_name(vim.api.nvim_get_current_buf())
+                    :gsub("/", "\\"),
+                config.get_root_dir()
+            )
+
+            local bufnr = vim.fn.bufnr(name, false)
+
+            local pos = { 1, 0 }
+            if bufnr ~= -1 then pos = vim.api.nvim_win_get_cursor(0) end
+
+            return {
+                value = name,
+                context = {
+                    row = pos[1],
+                    col = pos[2],
+                },
+            }
+        end,
     },
 })
 
