b78347759f06687279801955902b6a68dd984cee
oliwia <24637207+kkanden@users.noreply.github.com>
2025-04-06 22:26:07 +0200

    refactor(lazy): move luasnip configuration to separate file

--- lua/kanden/plugins/blink.lua
+++ lua/kanden/plugins/blink.lua
@@ -52,39 +52,9 @@ return {
         "Kaiser-Yang/blink-cmp-git",
         "disrupted/blink-cmp-conventional-commits",
         "mikavilpas/blink-ripgrep.nvim",
-        {
-            "L3MON4D3/LuaSnip",
-            lazy = true,
-            version = "v2.*",
-            dependencies = { "rafamadriz/friendly-snippets" },
-            build = "make install_jsregexp",
-            config = function()
-                local ls = require("luasnip")
 
-                require("luasnip.loaders.from_vscode").lazy_load()
-                require("luasnip.loaders.from_lua").lazy_load({
-                    paths = { vim.fn.stdpath("config") .. "/snippets" },
-                })
-                ls.filetype_extend("rmd", { "markdown" })
-
-                local map = require("kanden.lib").map
-
-                map({ "i", "s" }, "<Tab>", function()
-                    if ls.expand_or_jumpable() then ls.expand_or_jump() end
-                end, { silent = true })
-
-                map(
-                    { "i", "s" },
-                    "<S-Tab>",
-                    function() ls.jump(-1) end,
-                    { silent = true }
-                )
-
-                map({ "i", "s" }, "<C-s>", function()
-                    if ls.choice_active() then ls.change_choice(1) end
-                end, { silent = true })
-            end,
-        },
+        -- snippets
+        "L3MON4D3/LuaSnip",
     },
     opts = {
         keymap = {
