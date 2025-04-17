eab469624c0aa6ce7dc54cf28689aa767fea1df3
oliwia <24637207+kkanden@users.noreply.github.com>
2025-04-18 00:40:57 +0200

    luasnip: enable autosnippets

--- lua/kanden/plugins/luasnip.lua
+++ lua/kanden/plugins/luasnip.lua
@@ -7,6 +7,8 @@ return {
     config = function()
         local ls = require("luasnip")
 
+        ls.setup({ enable_autosnippets = true })
+
         require("luasnip.loaders.from_vscode").lazy_load()
         require("luasnip.loaders.from_lua").lazy_load({
             paths = { vim.fn.stdpath("config") .. "/snippets" },
