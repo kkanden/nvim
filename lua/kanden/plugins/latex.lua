f3a98cfdd1aa878e695166a52ae431a843e2741d
oliwia <24637207+kkanden@users.noreply.github.com>
2025-04-19 02:52:02 +0200

    vimtex: setup viewers

--- lua/kanden/plugins/latex.lua
+++ lua/kanden/plugins/latex.lua
@@ -3,9 +3,13 @@ return {
         "lervag/vimtex",
         init = function()
             vim.g.vimtex_imaps_enabled = 0
-            vim.g.vimtex_view_general_viewer = "sumatrapdf"
+            -- vim.g.vimtex_view_general_viewer = "sh ~/.local/bin/sumatrapdf"
+            -- vim.g.vimtex_view_general_options =
+            --     "-reuse-instance -forward-search @tex @line @pdf"
+
+            vim.g.vimtex_view_general_viewer = "okular"
             vim.g.vimtex_view_general_options =
-                "-reuse-instance -forward-search @tex @line @pdf"
+                "--unique file:@pdf#src:@line@tex"
         end,
     },
 }
