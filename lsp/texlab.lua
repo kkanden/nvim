fe0282f4ff4186a1e40ed863f1aba4d61fe89a1c
oliwia <24637207+kkanden@users.noreply.github.com>
2025-04-19 03:52:37 +0200

    latex: make vimtex and texlab os-specific

--- lsp/texlab.lua
+++ lsp/texlab.lua
@@ -1,18 +1,44 @@
+if vim.fn.has("win32") then
+    local executable = "sumatrapdf"
+    local args = {
+        "-reuse-instance",
+        "%p",
+        "-forward-search",
+        "%f",
+        "%l",
+    }
+else
+    local executable = "okular"
+    local args = { "--unique", "file:%p#src:%l%f" }
+end
+
 return {
     cmd = { "texlab" },
     filetypes = { "tex", "plaintex", "bib" },
     settings = {
         texlab = {
             rootDirectory = nil,
+            build = {
+                executable = "latexmk",
+                args = {
+                    "-pdf",
+                    "-interaction=nonstopmode",
+                    "-synctex=1",
+                    "%f",
+                },
+                onSave = true,
+                forwardSearchAfter = true,
+            },
             forwardSearch = {
-                executable = nil,
-                args = {},
+                executable = executable,
+                args = args,
             },
             chktex = {
                 onOpenAndSave = false,
                 onEdit = false,
             },
             diagnosticsDelay = 300,
+            formatterLineLength = 80,
         },
     },
 }
