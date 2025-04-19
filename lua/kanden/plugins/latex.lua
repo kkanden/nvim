0d8d60c4ddcca5ae8d6c2b03a058e0f674d72e71
oliwia <24637207+kkanden@users.noreply.github.com>
2025-04-19 03:52:37 +0200

    latex: make vimtex and texlab os-specific

--- lua/kanden/plugins/latex.lua
+++ lua/kanden/plugins/latex.lua
@@ -3,13 +3,15 @@ return {
         "lervag/vimtex",
         init = function()
             vim.g.vimtex_imaps_enabled = 0
-            -- vim.g.vimtex_view_general_viewer = "sh ~/.local/bin/sumatrapdf"
-            -- vim.g.vimtex_view_general_options =
-            --     "-reuse-instance -forward-search @tex @line @pdf"
-
-            vim.g.vimtex_view_general_viewer = "okular"
-            vim.g.vimtex_view_general_options =
-                "--unique file:@pdf#src:@line@tex"
+            if vim.fn.has("win32") then
+                vim.g.vimtex_view_general_viewer = "sumatrapdf"
+                vim.g.vimtex_view_general_options =
+                    "-reuse-instance -forward-search @tex @line @pdf"
+            else
+                vim.g.vimtex_view_general_viewer = "okular"
+                vim.g.vimtex_view_general_options =
+                    "--unique file:@pdf#src:@line@tex"
+            end
         end,
     },
 }
