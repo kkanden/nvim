0585f417ed4887f9a397c442b15936486e0966ad
oliwia <24637207+kkanden@users.noreply.github.com>
2025-04-21 18:33:03 +0200

    vimtex: change text objects mappings

--- lua/kanden/plugins/latex.lua
+++ lua/kanden/plugins/latex.lua
@@ -1,3 +1,5 @@
+local augroup = require("kanden.lib").augroup
+local map = require("kanden.lib").map
 return {
     {
         "lervag/vimtex",
@@ -12,6 +14,25 @@ return {
                 vim.g.vimtex_view_general_options =
                     "--unique file:@pdf#src:@line@tex"
             end
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "tex",
+                group = augroup("vimtex-mappings"),
+                callback = function()
+                    -- change math and item text objects mappings
+                    map({ "x", "o" }, "im", "<plug>(vimtex-i$)", { buffer = 0 })
+                    map({ "x", "o" }, "am", "<plug>(vimtex-a$)", { buffer = 0 })
+
+                    map({ "x", "o" }, "ii", "<plug>(vimtex-im)", { buffer = 0 })
+                    map({ "x", "o" }, "ai", "<plug>(vimtex-am)", { buffer = 0 })
+
+                    map(
+                        { "n" },
+                        "tsm",
+                        "<plug>(vimtex-env-toggle-math)",
+                        { buffer = 0 }
+                    )
+                end,
+            })
         end,
     },
 }
