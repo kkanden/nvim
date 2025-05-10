c7324d37b5eca9c2ea7bf028aeae2f92a7f88467
oliwia <24637207+kkanden@users.noreply.github.com>
2025-05-10 23:41:22 +0200

    lazy: add dev path option

--- lua/kanden/config/lazy.lua
+++ lua/kanden/config/lazy.lua
@@ -21,4 +21,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ spec = { import = "kanden.plugins" } })
+require("lazy").setup({
+    spec = { import = "kanden.plugins" },
+    dev = {
+        path = "~/code",
+    },
+})
