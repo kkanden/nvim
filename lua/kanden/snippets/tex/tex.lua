34949fc34624c54f05cf085016e7dec3bb439c6c
oliwia <24637207+kkanden@users.noreply.github.com>
2025-05-24 22:01:56 +0200

    snippets(tex): add snippets

--- lua/kanden/snippets/tex/tex.lua
+++ lua/kanden/snippets/tex/tex.lua
@@ -62,6 +62,29 @@ local generate_matrix = function(args, snip)
     nodes[#nodes] = t("\\\\")
     return sn(nil, nodes)
 end
+local generate_cases = function(args, snip)
+    local rows = tonumber(snip.captures[1]) or 2 -- default option 2 for cases
+    local cols = 2 -- fix to 2 cols
+    local nodes = {}
+    local ins_indx = 1
+    for j = 1, rows do
+        table.insert(nodes, r(ins_indx, tostring(j) .. "x1", i(1)))
+        ins_indx = ins_indx + 1
+        for k = 2, cols do
+            table.insert(nodes, t(" & "))
+            table.insert(
+                nodes,
+                r(ins_indx, tostring(j) .. "x" .. tostring(k), i(1))
+            )
+            ins_indx = ins_indx + 1
+        end
+        table.insert(nodes, t({ "\\\\", "" }))
+    end
+    -- fix last node.
+
+    table.remove(nodes, #nodes)
+    return sn(nil, nodes)
+end
 local matrix_nxn = s(
     {
         trig = "([%sbBpvV]?)mat(%d+)x(%d+)",
@@ -96,6 +119,25 @@ local matrix_nxn = s(
         }
     ),
     { condition = tex.in_math }
+)
+local cases = autosnippet(
+    {
+        trig = "(%d?)cases",
+        name = "cases",
+        dscr = "cases",
+        regTrig = true,
+        hidden = true,
+    },
+    fmta(
+        [[
+    \begin{cases}
+    <>
+    .\end{cases}
+    ]],
+        { d(1, generate_cases) }
+    ),
+
+    { condition = tex.in_math, show_condition = tex.in_math }
 )
 
 -- delimiters
@@ -181,8 +223,8 @@ local set = autosnippet(
         {
             c(1, {
                 r(1, ""),
-                sn(nil, { r(1, ""), t(" \\mid "), i(2) }),
                 sn(nil, { r(1, ""), t(" \\colon "), i(2) }),
+                sn(nil, { r(1, ""), t(" \\mid "), i(2) }),
             }),
             i(0),
         }
@@ -223,7 +265,7 @@ local draft = s(
     ]],
         { i(0) }
     ),
-    { condition = tex.not_math, show_condition = tex.not_math }
+    { condition = tex.in_preamble, show_condition = tex.in_preamble }
 )
 
 local homework = s(
@@ -276,7 +318,7 @@ local homework = s(
         ]],
         { i(1), i(2), i(0) }
     ),
-    { condition = tex.not_math, show_condition = tex.not_math }
+    { condition = tex.in_preamble, show_condition = tex.in_preamble }
 )
 
 local paired_delims = s(
@@ -291,11 +333,12 @@ local paired_delims = s(
     ]],
         { i(0) }
     ),
-    { condition = tex.not_math, show_condition = tex.not_math }
+    { condition = tex.in_preamble, show_condition = tex.in_preamble }
 )
 
 vim.list_extend(M, {
     matrix_nxn,
+    cases,
     infty,
     imath,
     dmath,
@@ -309,21 +352,6 @@ vim.list_extend(M, {
     paired_delims,
 })
 
-local auto_backslash_snippet = function(trig)
-    return autosnippet(
-        {
-            trig = trig,
-            desc = trig .. " with auto backslash",
-        },
-        fmta(
-            [[
-\<><>
-        ]],
-            { t(trig), i(0) }
-        ),
-        { condition = tex.in_math, show_condition = tex.in_math }
-    )
-end
 local auto_backslash_specs = {
     "arcsin",
     "sin",
@@ -353,9 +381,171 @@ local auto_backslash_specs = {
 
 local auto_backslash_snippets = {}
 for _, v in ipairs(auto_backslash_specs) do
-    table.insert(auto_backslash_snippets, auto_backslash_snippet(v))
+    table.insert(
+        auto_backslash_snippets,
+        tex.auto_backslash(
+            { trig = v },
+            { condition = tex.in_math, show_condition = tex.in_math }
+        )
+    )
 end
 vim.list_extend(M, auto_backslash_snippets)
+
+-- greek letters
+local greek_specs = {
+    alpha = { context = { name = "α" }, command = [[\alpha]] },
+    beta = { context = { name = "β" }, command = [[\beta]] },
+    gam = { context = { name = "γ" }, command = [[\gamma]] },
+    Gam = { context = { name = "Γ" }, command = [[\Gamma]] },
+    delta = { context = { name = "δ" }, command = [[\delta]] },
+
+    DD = { context = { name = "Δ" }, command = [[\Delta]] },
+    eps = { context = { name = "ε", priority = 500 }, command = [[\epsilon]] },
+    veps = { context = { name = "ε" }, command = [[\varepsilon]] },
+    zeta = { context = { name = "ζ" }, command = [[\zeta]] },
+    eta = { context = { name = "η", priority = 500 }, command = [[\eta]] },
+    theta = { context = { name = "θ" }, command = [[\theta]] },
+    Theta = { context = { name = "Θ" }, command = [[\Theta]] },
+    iota = { context = { name = "ι" }, command = [[\iota]] },
+    kappa = { context = { name = "κ" }, command = [[\kappa]] },
+    lmbd = { context = { name = "λ" }, command = [[\lambda]] },
+    Lmbd = { context = { name = "Λ" }, command = [[\Lambda]] },
+    mu = { context = { name = "μ" }, command = [[\mu]] },
+    nu = { context = { name = "ν" }, command = [[\nu]] },
+    xi = { context = { name = "ξ" }, command = [[\xi]] },
+    pi = { context = { name = "π" }, command = [[\pi]] },
+    rho = { context = { name = "ρ" }, command = [[\rho]] },
+    sig = { context = { name = "σ" }, command = [[\sigma]] },
+    Sig = { context = { name = "Σ" }, command = [[\Sigma]] },
+    tau = { context = { name = "τ" }, command = [[\tau]] },
+    ups = { context = { name = "υ" }, command = [[\upsilon]] },
+    phi = { context = { name = "φ" }, command = [[\phi]] },
+    vphi = { context = { name = "φ" }, command = [[\varphi]] },
+    chi = { context = { name = "χ" }, command = [[\chi]] },
+    psi = { context = { name = "Ψ" }, command = [[\psi]] },
+    omega = { context = { name = "ω" }, command = [[\omega]] },
+    Omega = { context = { name = "Ω" }, command = [[\Omega]] },
+}
+
+local greek_snippets = {}
+for k, v in pairs(greek_specs) do
+    table.insert(
+        greek_snippets,
+        tex.symbol_snippet(
+            vim.tbl_deep_extend("keep", { trig = k }, v.context),
+            v.command,
+            {
+                condition = tex.in_math,
+                show_condition = tex.in_math,
+                backslash = true,
+            }
+        )
+    )
+end
+vim.list_extend(M, greek_snippets)
+
+-- symbols
+local symbol_specs = {
+    -- operators
+    ["!="] = { context = { name = "!=" }, command = [[\neq]] },
+    ["<="] = { context = { name = "≤" }, command = [[\leq]] },
+    [">="] = { context = { name = "≥" }, command = [[\geq]] },
+    ["<<"] = { context = { name = "<<" }, command = [[\ll]] },
+    [">>"] = { context = { name = ">>" }, command = [[\gg]] },
+    ["~~"] = { context = { name = "~" }, command = [[\sim]] },
+
+    ["~="] = { context = { name = "≈" }, command = [[\approx]] },
+    ["~-"] = { context = { name = "≃" }, command = [[\simeq]] },
+    ["-~"] = { context = { name = "⋍" }, command = [[\backsimeq]] },
+
+    ["-="] = { context = { name = "≡" }, command = [[\equiv]] },
+    ["=~"] = { context = { name = "≅" }, command = [[\cong]] },
+    [":="] = { context = { name = "≔" }, command = [[\definedas]] },
+    ["**"] = { context = { name = "·", priority = 100 }, command = [[\cdot]] },
+    xx = { context = { name = "×" }, command = [[\times]] },
+    ["!+"] = { context = { name = "⊕" }, command = [[\oplus]] },
+    ["!*"] = { context = { name = "⊗" }, command = [[\otimes]] },
+    -- sets
+    NN = { context = { name = "ℕ" }, command = [[\mathbb{N}]] },
+    ZZ = { context = { name = "ℤ" }, command = [[\mathbb{Z}]] },
+    QQ = { context = { name = "ℚ" }, command = [[\mathbb{Q}]] },
+    RR = { context = { name = "ℝ" }, command = [[\mathbb{R}]] },
+    CC = { context = { name = "ℂ" }, command = [[\mathbb{C}]] },
+    OO = { context = { name = "∅" }, command = [[\varnothing]] },
+    pwr = { context = { name = "P" }, command = [[\powerset]] },
+    cc = { context = { name = "⊂" }, command = [[\subset]] },
+
+    cq = { context = { name = "⊆" }, command = [[\subseteq]] },
+    qq = { context = { name = "⊃" }, command = [[\supset]] },
+    qc = { context = { name = "⊇" }, command = [[\supseteq]] },
+    ["\\\\\\"] = { context = { name = "⧵" }, command = [[\setminus]] },
+    Nn = { context = { name = "∩" }, command = [[\cap]] },
+    UU = { context = { name = "∪" }, command = [[\cup]] },
+    ["::"] = { context = { name = ":" }, command = [[\colon]] },
+    -- quantifiers and logic stuffs
+    AA = { context = { name = "∀" }, command = [[\forall]] },
+    EE = { context = { name = "∃" }, command = [[\exists]] },
+    inn = { context = { name = "∈" }, command = [[\in]] },
+    notin = { context = { name = "∉" }, command = [[\not\in]] },
+    ["!-"] = { context = { name = "¬" }, command = [[\lnot]] },
+    VV = { context = { name = "∨" }, command = [[\lor]] },
+    WW = { context = { name = "∧" }, command = [[\land]] },
+    ["!W"] = { context = { name = "∧" }, command = [[\bigwedge]] },
+    ["=>"] = { context = { name = "⇒" }, command = [[\implies]] },
+    ["=<"] = { context = { name = "⇐" }, command = [[\impliedby]] },
+    iff = { context = { name = "⟺" }, command = [[\iff]] },
+    ["->"] = { context = { name = "→", priority = 250 }, command = [[\to]] },
+    ["!>"] = { context = { name = "↦" }, command = [[\mapsto]] },
+    ["<-"] = { context = { name = "↦", priority = 250 }, command = [[\gets]] },
+    -- differentials
+    dp = { context = { name = "⇐" }, command = [[\partial]] },
+    -- arrows
+    ["-->"] = {
+        context = { name = "⟶", priority = 500 },
+        command = fmta([[\xrightarrow[<>]{<>}<>]], {
+            c(1, { i(1, "below"), t("") }),
+            c(2, { i(2, "above"), t("") }),
+            i(0),
+        }),
+    },
+    ["<--"] = {
+        context = { name = "⟶", priority = 500 },
+        command = fmta([[\xleftarrow[<>]{<>}<>]], {
+            c(1, { i(1, "below"), t("") }),
+            c(2, { i(2, "above"), t("") }),
+            i(0),
+        }),
+    },
+    ["<->"] = {
+        context = { name = "↔", priority = 500 },
+        command = [[\xleftrightarrow]],
+    },
+    ["2>"] = {
+        context = { name = "⇉", priority = 400 },
+        command = [[\rightrightarrows]],
+    },
+    upar = { context = { name = "↑" }, command = [[\uparrow]] },
+    dnar = { context = { name = "↓" }, command = [[\downarrow]] },
+    -- etc
+    oo = { context = { name = "∞" }, command = [[\infty]] },
+    lll = { context = { name = "ℓ" }, command = [[\ell]] },
+    dag = { context = { name = "†" }, command = [[\dagger]] },
+    ["+-"] = { context = { name = "†" }, command = [[\pm]] },
+    ["-+"] = { context = { name = "†" }, command = [[\mp]] },
+}
+
+local symbol_snippets = {}
+for k, v in pairs(symbol_specs) do
+    table.insert(
+        symbol_snippets,
+        tex.symbol_snippet(
+            vim.tbl_deep_extend("keep", { trig = k }, v.context),
+            v.command,
+            { condition = tex.in_math, show_condition = tex.in_math }
+        )
+    )
+end
+vim.list_extend(M, symbol_snippets)
 
 -- postifx
 local postfix_math_specs = {
@@ -420,15 +610,17 @@ local postfix_math_specs = {
             post = [[}]],
         },
     },
+    hat = {
+        context = {
+            name = "hat",
+            dscr = "hat",
+        },
+        command = {
+            pre = [[\hat{]],
+            post = [[}]],
+        },
+    },
 }
-
-local hat = postfix({
-    trig = "hat",
-    match_pattern = [[[\\%w%.%_%-%"%']+$]],
-    snippetType = "autosnippet",
-    dscr = "postfix hat when in math mode",
-}, { l("\\hat{" .. l.POSTFIX_MATCH .. "}") }, { condition = tex.in_math })
-vim.list_extend(M, { hat })
 
 local postfix_math_snippets = {}
 for k, v in pairs(postfix_math_specs) do
--- lua/kanden/snippets/tex/utils/utils.lua
+++ lua/kanden/snippets/tex/utils/utils.lua
@@ -1,14 +1,18 @@
 local ls = require("luasnip")
+local s = ls.snippet
 local sn = ls.snippet_node
 local t = ls.text_node
 local i = ls.insert_node
 local d = ls.dynamic_node
+local f = ls.function_node
 local fmta = require("luasnip.extras.fmt").fmta
 local postfix = require("luasnip.extras.postfix").postfix
+local autosnippet =
+    ls.extend_decorator.apply(s, { snippetType = "autosnippet" })
 
 local M = {}
 
-local env = function(name)
+M.in_env = function(name)
     local is_inside = vim.fn["vimtex#env#is_inside"](name)
     return (is_inside[1] > 0 and is_inside[2] > 0)
 end
@@ -23,11 +27,11 @@ M.in_math = function() return ts_utils.in_mathzone() end
 
 M.not_math = function() return not M.in_math() end
 
-M.in_text = function() return env("document") and M.not_math end
+M.in_text = function() return M.in_env("document") and M.not_math end
 
 M.comment = function() return vim.fn["vimtex#syntax#in_comment"]() == 1 end
 
-M.in_preamble = function() return not env("document") end
+M.in_preamble = function() return not M.in_env("document") end
 
 M.get_visual = function(args, parent)
     if #parent.snippet.env.SELECT_RAW > 0 then
@@ -79,13 +83,10 @@ M.postfix_snippet = function(context, command, opts)
     context.docstring = command.pre
         .. [[(POSTFIX_MATCH|VISUAL|<1>)]]
         .. command.post
-    context.match_pattern = [[[%w%.%_%-%"%']*$]]
-    local j, _ = string.find(command.pre, context.trig)
-    if j == 2 then
-        context.trigEngine = "ecma"
-        context.trig = "(?<!\\\\)" .. "(" .. context.trig .. ")"
-        context.hidden = true
-    end
+    context.match_pattern = [[[%w%.%_%-%"%'\\]*$]]
+    context.trigEngine = "ecma"
+    context.trig = string.format("(?<!\\\\)(%s)", context.trig)
+    context.hidden = true
     return postfix(context, {
         d(
             1,
@@ -94,6 +95,48 @@ M.postfix_snippet = function(context, command, opts)
             { user_args = { command.pre, command.post } }
         ),
     }, opts)
+end
+
+M.auto_backslash = function(context, opts)
+    opts = opts or {}
+    if not context.trig then
+        error("context doesn't include a `trig` key which is mandatory", 2)
+    end
+    context.dscr = context.dscr or (context.trig .. "with automatic backslash")
+    context.name = context.name or context.trig
+    context.docstring = context.docstring or ([[\]] .. context.trig)
+    context.trigEngine = "ecma"
+    context.trig = string.format("(?<!\\\\)(%s)", context.trig)
+    return autosnippet(
+        context,
+        fmta(
+            [[\<><>]],
+            { f(function(_, snip) return snip.captures[1] end), i(0) }
+        ),
+        opts
+    )
+end
+
+M.symbol_snippet = function(context, command, opts)
+    opts = opts or {}
+    if not context.trig then
+        error("context doesn't include a `trig` key which is mandatory", 2)
+    end
+    context.name = context.name or command:gsub([[\]], "")
+    context.dscr = context.dscr or context.name
+    context.docstring = context.docstring or context.name
+    context.wordTrig = context.wordTrig or false
+    local exec = command
+    if type(command) == "string" then -- if it's string, take it literally
+        local j, _ = string.find(command, context.trig)
+        if j == 2 then -- command always starts with backslash
+            context.trigEngine = "ecma"
+            context.trig = string.format("(?<!\\\\)(%s)", context.trig)
+            context.hidden = true
+        end
+        exec = t(command)
+    end
+    return autosnippet(context, exec, opts)
 end
 
 return M
