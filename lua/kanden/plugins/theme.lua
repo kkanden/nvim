11022e08bbfa2f80f286c7a303387b0019c64b21
oliwia <24637207+kkanden@users.noreply.github.com>
2025-06-03 21:10:10 +0200

    theme(vague): add blink highlights

--- lua/kanden/plugins/theme.lua
+++ lua/kanden/plugins/theme.lua
@@ -176,6 +176,42 @@ return {
             transparent = false,
             bold = false,
             italic = false,
+            on_highlights = function(_, colors)
+                local highlights = {
+                    BlinkCmpKindClass = { fg = colors.number },
+                    BlinkCmpKindConstant = { fg = colors.parameter },
+                    BlinkCmpKindConstructor = {},
+                    BlinkCmpKindEnum = { fg = colors.number },
+                    BlinkCmpKindEnumMember = { fg = colors.plus },
+                    BlinkCmpKindEvent = { fg = colors.number },
+                    BlinkCmpKindField = { fg = colors.keyword },
+                    BlinkCmpKindFunction = { fg = colors.hint },
+                    BlinkCmpKindInterface = { fg = colors.number },
+                    BlinkCmpKindKeyword = { fg = colors.error },
+                    BlinkCmpKindMethod = { fg = colors.hint },
+                    BlinkCmpKindModule = { fg = colors.warning },
+                    BlinkCmpKindOperator = { fg = colors.keyword },
+                    BlinkCmpKindProperty = { fg = colors.keyword },
+                    BlinkCmpKindReference = { fg = colors.parameter },
+                    BlinkCmpKindSnippet = { fg = colors.keyword },
+                    BlinkCmpKindStruct = { fg = colors.number },
+                    BlinkCmpKindTypeParameter = { fg = colors.hint },
+                    BlinkCmpKindUnit = { fg = colors.number },
+                    BlinkCmpKindVariable = { fg = colors.parameter },
+                }
+                for name, setting in pairs(highlights) do
+                    vim.api.nvim_command(
+                        string.format(
+                            "highlight %s guifg=%s guibg=%s guisp=%s gui=%s",
+                            name,
+                            setting.fg or "none",
+                            setting.bg or "none",
+                            setting.sp or "none",
+                            setting.gui or "none"
+                        )
+                    )
+                end
+            end,
         },
     },
 }
