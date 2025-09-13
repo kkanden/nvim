5fd0ac4ad66ab27090ec1daae44d8e8f95edf249
oliwia <24637207+kkanden@users.noreply.github.com>
2025-09-14 01:02:56 +0200

    treesitter: clean up configuration

--- lua/kanden/plugins/treesitter.lua
+++ lua/kanden/plugins/treesitter.lua
@@ -4,27 +4,8 @@ return {
         event = { "BufReadPost", "BufNewFile" },
         build = ":TSUpdate",
         opts = {
-            ensure_installed = {
-                "c",
-                "lua",
-                "vim",
-                "vimdoc",
-                "query",
-                "markdown",
-                "markdown_inline",
-                "r",
-                "rnoweb",
-                "yaml",
-                "latex",
-            },
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
-
+            auto_install = false,
             highlight = {
                 enable = true,
 
