9889fc6ccd9247a6b6aaf7e2e72e658aeef0a7df
oliwia <24637207+kkanden@users.noreply.github.com>
2025-09-22 20:46:23 +0200

    remove unused pluins

--- lua/kanden/plugins/markdown.lua
+++ lua/kanden/plugins/markdown.lua
@@ -1,48 +1,6 @@
-local map = require("kanden.lib").map
-local augroup = require("kanden.lib").augroup
-
 local fts = { "markdown", "rmd" }
 
 return {
-    {
-        "MeanderingProgrammer/render-markdown.nvim",
-        ft = fts,
-        enabled = false,
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            "echasnovski/mini.icons",
-        },
-        ---@module 'render-markdown'
-        ---@type render.md.UserConfig
-        opts = {
-            enabled = false, -- don't autostart
-            render_modes = true, -- render in all modes, inc. insert, reduces dizziness
-            file_types = fts,
-            completions = {
-                lsp = {
-                    enabled = true,
-                },
-            },
-            latex = {
-                enabled = true,
-            },
-        },
-        init = function()
-            vim.api.nvim_create_autocmd("FileType", {
-                group = augroup("markdown-toggle"),
-                pattern = fts,
-                callback = function()
-                    map(
-                        "n",
-                        "<leader>mt",
-                        "<Cmd>RenderMarkdown toggle<CR>",
-                        { buffer = 0 }
-                    )
-                end,
-            })
-        end,
-    },
-
     {
         "iamcco/markdown-preview.nvim",
         ft = fts,
