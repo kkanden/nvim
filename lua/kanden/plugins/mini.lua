ba91b55e868db834d52e2e6192ff2b65c44e9013
oliwia <24637207+kkanden@users.noreply.github.com>
2025-09-23 16:51:16 +0200

    switch to mini.statusline because ummm because.... uhmmmm

--- lua/kanden/plugins/mini.lua
+++ lua/kanden/plugins/mini.lua
@@ -102,4 +102,63 @@ return {
             },
         },
     },
+    {
+        "nvim-mini/mini.statusline",
+        opts = {
+            content = {
+                active = function()
+                    local mode, mode_hl =
+                        require("mini.statusline").section_mode({
+                            trunc_width = 0,
+                        })
+                    mode = mode:upper()
+                    local git = require("mini.statusline").section_git({
+                        trunc_width = 0,
+                    })
+                    local filename =
+                        require("mini.statusline").section_filename({
+                            trunc_width = 1e6,
+                        })
+                    local fileinfo =
+                        require("mini.statusline").section_fileinfo({
+                            trunc_width = 1e6,
+                        })
+                    local location =
+                        require("mini.statusline").section_location({
+                            trunc_width = 0,
+                        })
+
+                    local lsp = table.concat(
+                        vim.iter(vim.lsp.get_clients({ bufnr = 0 }))
+                            :map(function(x) return x.name end)
+                            :totable(),
+                        ", "
+                    )
+                    lsp = lsp ~= "" and lsp .. " | " or lsp
+
+                    return require("mini.statusline").combine_groups({
+                        { hl = mode_hl, strings = { mode } },
+                        {
+                            hl = "MiniStatuslineDevinfo",
+                            strings = { git },
+                        },
+                        "%<", -- Mark general truncate point
+                        {
+                            hl = "MiniStatuslineFilename",
+                            strings = { filename },
+                        },
+                        "%=", -- End left alignment
+                        {
+                            hl = "MiniStatuslineFileinfo",
+                            strings = { lsp .. fileinfo },
+                        },
+                        {
+                            hl = mode_hl,
+                            strings = { location },
+                        },
+                    })
+                end,
+            },
+        },
+    },
 }
