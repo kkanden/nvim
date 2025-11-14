2118b7b7ee16014560f414cfb82f3a79acf32cdd
oliwia <24637207+kkanden@users.noreply.github.com>
2025-11-14 23:01:13 +0100

    treesitter: fixfixifx

---
+++ after/plugin/nixos-plugins.lua
@@ -0,0 +1,15 @@
+if vim.uv.os_uname().version:lower():match("nixos") then
+    vim.cmd("packadd nvim-treesitter")
+
+    require("nvim-treesitter.configs").setup({
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
+        sync_install = false,
+        auto_install = false,
+        indent = {
+            enable = true,
+        },
+    })
+end
