2989d1d11a6a24e11c90b8deef20a8a3d99fa39c
oliwia <24637207+kkanden@users.noreply.github.com>
2026-03-03 00:03:09 +0100

    initial form of lazy loading

--- lua/kanden/config/pack.lua
+++ lua/kanden/config/pack.lua
@@ -3,16 +3,22 @@ local function gh(x) return "https://github.com/" .. x end
 local plugins = {
     gh("windwp/nvim-ts-autotag"),
 
-    { src = gh("saghen/blink.cmp"), version = vim.version.range("*") },
-    { src = gh("L3MON4D3/LuaSnip"), version = vim.version.range("*") },
+    {
+        src = gh("saghen/blink.cmp"),
+        version = vim.version.range("*"),
+    },
+    {
+        src = gh("L3MON4D3/LuaSnip"),
+        version = vim.version.range("*"),
+    },
     gh("rafamadriz/friendly-snippets"),
     gh("mikavilpas/blink-ripgrep.nvim"),
 
-    gh("catgoose/nvim-colorizer.lua"),
+    -- gh("catgoose/nvim-colorizer.lua"),
 
     gh("stevearc/conform.nvim"),
 
-    gh("j-hui/fidget.nvim"),
+    { src = gh("j-hui/fidget.nvim"), data = { now = true } },
 
     gh("tpope/vim-fugitive"),
 
@@ -24,22 +30,25 @@ local plugins = {
 
     gh("lukas-reineke/indent-blankline.nvim"),
 
-    gh("folke/lazydev.nvim"),
+    { src = gh("folke/lazydev.nvim"), data = { ft = { "lua" } } },
 
-    gh("selimacerbas/markdown-preview.nvim"),
+    {
+        src = gh("selimacerbas/markdown-preview.nvim"),
+        data = { ft = { "markdown" } },
+    },
     gh("selimacerbas/live-server.nvim"),
 
     gh("nvim-mini/mini.nvim"),
 
     gh("kkanden/minipoon.nvim"),
 
-    gh("stevearc/oil.nvim"),
+    { src = gh("stevearc/oil.nvim"), data = { now = true } },
 
-    gh("R-nvim/R.nvim"),
+    { src = gh("R-nvim/R.nvim"), data = { ft = { "r", "rmd" } } },
 
     gh("kylechui/nvim-surround"),
 
-    gh("vague2k/vague.nvim"),
+    { src = gh("vague2k/vague.nvim"), data = { now = true } },
 
     gh("nguyenvukhang/nvim-toggler"),
 
@@ -51,12 +60,52 @@ vim.list_extend(
     not vim.g.on_nixos and { gh("nvim-treesitter/nvim-treesitter") } or {}
 )
 
+---@param plugin string
+local function normalize_name(plugin)
+    local basename = plugin
+        :lower()
+        :match("https://.+/.+/([%w_%-%.]+)")
+        :gsub("%.%w+$", "")
+        :gsub("n?vim%-", "")
+    return basename
+end
+
+plugins = vim.iter(plugins)
+    :map(function(x)
+        if type(x) == "string" then
+            return { src = x, name = normalize_name(x) }
+        elseif type(x) == "table" then
+            return vim.tbl_extend("keep", x, { name = normalize_name(x.src) })
+        end
+    end)
+    :totable()
+
 vim.pack.add(plugins)
 
 -- source setups
 local plugins_dir =
     vim.fs.joinpath(vim.fn.stdpath("config"), "lua/kanden/plugins")
 
+-- based on MiniMax config
+local misc = require("mini.misc")
+local now = function(f) misc.safely("now", f) end
+local lazy = function(f) misc.safely("later", f) end
+local on_filetype = function(ft, f) misc.safely("filetype:" .. ft, f) end
+
 for name, _ in vim.fs.dir(plugins_dir) do
-    dofile(vim.fs.joinpath(plugins_dir, name))
+    local setup = function() dofile(vim.fs.joinpath(plugins_dir, name)) end
+
+    local basename = name:gsub("%.lua", "")
+    local spec = vim.iter(plugins)
+        :filter(function(x) return x.name == basename end)
+        :totable()[1] or {}
+
+    -- plugins are lazy by default
+    if spec.data and spec.data.now then
+        now(setup)
+    elseif spec.data and spec.data.ft then
+        on_filetype(table.concat(spec.data.ft, ","), setup)
+    else
+        lazy(setup)
+    end
 end
