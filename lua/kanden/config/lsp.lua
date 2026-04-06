8902d5eaa2875dd2fb8c73fa1ee4b3e4c50e6352
oliwia <24637207+kkanden@users.noreply.github.com>
2026-04-06 18:14:19 +0200

    lsp: lspprogress

--- lua/kanden/config/lsp.lua
+++ lua/kanden/config/lsp.lua
@@ -30,6 +30,21 @@ vim.api.nvim_create_autocmd("LspAttach", {
             function() require("mini.extra").pickers.diagnostic() end,
             "Diagnostics"
         )
+
+        vim.api.nvim_create_autocmd("LspProgress", {
+            buffer = args.buf,
+            callback = function(ev)
+                local value = ev.data.params.value
+                vim.api.nvim_echo({ { value.message or "done" } }, false, {
+                    id = "lsp." .. ev.data.client_id,
+                    kind = "progress",
+                    source = "vim.lsp",
+                    title = value.title,
+                    status = value.kind ~= "end" and "running" or "success",
+                    percent = value.percentage,
+                })
+            end,
+        })
     end,
 })
 
