6a9f123f651ac979a6f782256d08f19d892696bf
oliwia <24637207+kkanden@users.noreply.github.com>
2026-04-06 18:15:54 +0200

    complete: pretty

--- lua/kanden/config/complete.lua
+++ lua/kanden/config/complete.lua
@@ -1,8 +1,9 @@
 local map = require("kanden.lib").map
 local augroup = require("kanden.lib").augroup
 
-vim.o.complete = ".,w,b,F,o"
-vim.o.completeopt = "popup,menuone,noinsert,fuzzy"
+vim.o.complete = "F,o"
+vim.o.completeitemalign = "kind,abbr,menu"
+vim.o.completeopt = "popup,menuone,fuzzy"
 
 map({ "i", "c" }, "<C-b>", "pumvisible() ? '<C-y>' : '<C-b>'", { expr = true })
 
@@ -12,7 +13,40 @@ vim.o.wildoptions = "fuzzy"
 vim.o.pumborder = "single"
 vim.o.pumheight = 10
 vim.o.pumwidth = 15
-vim.o.pummaxwidth = 30
+vim.o.pummaxwidth = 50
+
+kind_map = {}
+for k, v in pairs(vim.lsp.protocol.CompletionItemKind) do
+    if type(k) == "string" and type(v) == "number" then kind_map[v] = k end
+end
+
+kind_icons = {
+    Class = "󱡠",
+    Color = "󰏘",
+    Constant = "󰏿",
+    Constructor = "󰒓",
+    Enum = "󰦨",
+    EnumMember = "󰦨",
+    Event = "󱐋",
+    Field = "󰜢",
+    File = "󰈔",
+    Folder = "󰉋",
+    Function = "󰊕",
+    Interface = "󱡠",
+    Keyword = "󰻾",
+    Method = "󰊕",
+    Module = "󰅩",
+    Operator = "󰪚",
+    Property = "󰖷",
+    Reference = "󰬲",
+    Snippet = "󱄽",
+    Struct = "󱡠",
+    Text = "󰉿",
+    TypeParameter = "󰬛",
+    Unit = "󰪚",
+    Value = "󰦨",
+    Variable = "󰆦",
+}
 
 vim.api.nvim_create_autocmd("LspAttach", {
     group = augroup("lsp_complete"),
@@ -20,12 +54,21 @@ vim.api.nvim_create_autocmd("LspAttach", {
         local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
         if client:supports_method("textDocument/completion") then
-            vim.lsp.completion.enable(
-                true,
-                client.id,
-                args.buf,
-                { autotrigger = false }
-            )
+            vim.lsp.completion.enable(true, client.id, args.buf, {
+                autotrigger = false,
+                convert = function(item)
+                    local kind = kind_map[item.kind]
+                    local icon = kind_icons[kind]
+                    local _, hl, _ =
+                        require("mini.icons").get("lsp", kind or "Unknown")
+                    return {
+                        kind = icon,
+                        kind_hlgroup = hl,
+                        abbr = item.label,
+                        menu = item.documentation,
+                    }
+                end,
+            })
         end
     end,
 })
