175a1940806a7f5a516ff817a0e26ffff08a51c7
oliwia <24637207+kkanden@users.noreply.github.com>
2026-04-17 14:38:19 +0200

    remove unused lib functions

--- lua/kanden/lib.lua
+++ lua/kanden/lib.lua
@@ -7,28 +7,4 @@ M.augroup = function(name)
     return vim.api.nvim_create_augroup("oliwia_" .. name, { clear = true })
 end
 
----Run OS command
----@param cmd string[]
----@param opts? any
----@return string?
----@return integer
----@return string?
-M.system_output = function(cmd, opts)
-    opts = opts or {}
-    local obj = vim.system(cmd, opts):wait()
-    return obj.stdout, obj.code, obj.stderr
-end
-
-M.user_cmd_complete = function(completions)
-    return function(arg_lead, cmd_line, cursor_pos)
-        local matches = {}
-        for _, asset in ipairs(completions) do
-            if asset:lower():find(arg_lead:lower(), 1, true) then
-                table.insert(matches, asset)
-            end
-        end
-        return matches
-    end
-end
-
 return M
