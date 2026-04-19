da3e4d2c80d8253cac049f9196f736fd093092c4
oliwia <24637207+kkanden@users.noreply.github.com>
2026-04-19 13:20:48 +0200

    ftplugin(rmd): move the folding here

--- after/ftplugin/rmd.lua
+++ after/ftplugin/rmd.lua
@@ -1,3 +1,15 @@
 vim.bo.shiftwidth = 2
 vim.wo.foldexpr = "v:lua.RFoldComment(v:lnum)"
 vim.wo.foldmethod = "expr"
+
+function RFoldComment(lnum)
+    local line = vim.fn.getline(lnum)
+    if string.match(line, "^```{?%w+") then
+        return ">1"
+    elseif string.match(line, "^```") then
+        return "<1"
+    elseif string.match(line, "^#%s*.*%s*-+%s*#*$") then
+        return ">2"
+    end
+    return "="
+end
