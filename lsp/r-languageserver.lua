4bdc1237780186250686f6a80bfcf9536e65fbbb
oliwia <24637207+kkanden@users.noreply.github.com>
2026-04-22 14:30:44 +0200

    lsp(r): disable linter

--- lsp/r-languageserver.lua
+++ lsp/r-languageserver.lua
@@ -2,4 +2,7 @@ return {
     cmd = { "R", "--slave", "-e", "languageserver::run()" },
     filetypes = { "r", "rmd" },
     root_markers = { ".Rprofile" },
+    settings = {
+        r = { lsp = { diagnostics = false } },
+    },
 }
